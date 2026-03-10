d3bda7aade7fb79656f735c1a34a4dbc7526ab46
lisayinr <lisayin@byui.edu>
2026-03-10 17:02:00 -0600

    completed w10 prove

--- ponder/sortfilter.js
+++ ponder/sortfilter.js
@@ -22,4 +22,81 @@ let searchTerm = 'co';
 let filterFruit = lowerSort.filter(searchFruit);
 
 function searchFruit(item) {
-    re
+    return item.includes(searchTerm);
+}
+
+console.log(filterFruit);
+
+
+function compareFn(a,b) {
+  if (a.productName < b.productName) {
+    return -1;
+  } else if (a.productName > b.productName) {
+    return 1;
+  }
+ return 0;
+}
+
+const products = [
+  {
+    productName: "Wireless Mouse",
+    price: 29.99
+  },
+  {
+    productName: "Bluetooth Keyboard",
+    price: 49.99
+  },
+  {
+    productName: "Laptop Stand",
+    price: 39.99
+  }
+];
+
+let productSort = products.sort(compareFn);
+
+console.log(productSort);
+                
+
+const animals = [
+  {
+    name: "Lion",
+    traits: ["brave", "strong", "fierce", "wild"]
+  },
+  {
+    name: "Elephant",
+    traits: ["large", "gentle", "smart", "wild"]
+  },
+  {
+    name: "Fox",
+    traits: ["sly", "quick", "clever", "wild"]
+  },
+  {
+    name: "Dog",
+    traits: ["loyal", "friendly", "playful", "cuddly"]
+  },
+  {
+    name: "Cat",
+    traits: ["quiet", "independent", "curious", "cuddly"]
+  }
+];
+              
+let query = 'ox';
+
+let filteredList = animals.filter(searchList);
+
+function searchList(item) {
+    return item.name.toLowerCase().includes(query.toLowerCase());
+}
+
+console.log(filteredList);
+
+
+let queryTrait = 'strong';
+
+let filteredTraits = animals.filter(searchTraits);
+
+function searchTraits(item) {
+    return item.traits.find((trait) => trait.toLowerCase().includes(queryTrait.toLowerCase()));
+}
+
+console.log(filteredTraits);
